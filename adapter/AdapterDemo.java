bd53227a8d5ee7b78803718bef7d62b0d9fce7a2
Seshareddy <kamaganiguntlaseshareddy@gmail.com>
2020-11-03 09:49:52 +0530

    Design Patterns

---
+++ adapter/AdapterDemo.java
@@ -0,0 +1,77 @@
+package com.company.dp1.adapter;
+
+interface Bird
+{
+    // birds implement Bird interface that allows
+    // them to fly and make sounds adaptee interface
+
+    void fly();
+    void makeSound();
+}
+
+class Sparrow implements Bird
+{
+    // a concrete implementation of bird
+    public void fly()
+    {
+        System.out.println("Flying");
+    }
+    public void makeSound()
+    {
+        System.out.println("Chirp Chirp");
+    }
+}
+
+interface ToyDuck
+{
+
+    void squeak();
+}
+
+class PlasticToyDuck implements ToyDuck
+{
+    public void squeak()
+    {
+        System.out.println("Squeak");
+    }
+}
+
+class BirdAdapter implements ToyDuck
+{
+    // You need to implement the interface your
+    // client expects to use.
+    Bird bird;
+    public BirdAdapter(Bird bird)
+    {
+        // we need reference to the object we
+        // are adapting
+        this.bird = bird;
+    }
+
+    public void squeak()
+    {
+        // translate the methods appropriately
+        bird.makeSound();
+    }
+}
+
+
+public class AdapterDemo {
+    public static void main(String args[]) {
+        Sparrow sparrow = new Sparrow();
+        ToyDuck toyDuck = new PlasticToyDuck();
+        ToyDuck birdAdapter = new BirdAdapter(sparrow);
+
+        System.out.println("Sparrow...");
+
+        sparrow.fly();
+        sparrow.makeSound();
+
+        System.out.println("ToyDuck...");
+        toyDuck.squeak();
+
+        // toy duck behaving like a bird
+        System.out.println("BirdAdapter...");
+        birdAdapter.squeak();
+    }
+}
